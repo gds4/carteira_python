623da168090834359c1e5071192e06c898a2f144
jvsn <jvsn.10.jv@gmail.com>
2025-02-17 11:43:44 -0300

    Adicionado chache em banco de dados

--- carteira/carteira/settings.py
+++ carteira/carteira/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'django_cache',
+    }
+}
