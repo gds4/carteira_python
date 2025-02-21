f43331da75df048d1f6977c1bfcbf4be5cc61d6f
jvsn <jvsn.10.jv@gmail.com>
2025-02-21 12:17:08 -0300

    Adiciona redirecionamento para a página de login e melhora a responsividade da tabela de ativos.

--- carteira/carteira/urls.py
+++ carteira/carteira/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ativos/', include('ativos.urls')),
     path('usuarios/', include('usuarios.urls')),
+    path('', lambda request: redirect('login', permanent=False))
 ]
